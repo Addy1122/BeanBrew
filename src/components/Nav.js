9116ef3192af4d9826af6e7e8e54f5ada745b88f
Adline Samuel <adlinesamuel@Adlines-MacBook-Air-2.local>
2024-07-31 00:56:13 -0400

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import beanBrewImage from '../assets/BeanBrew2.png';
+
+const Nav = () => {
+  return (
+      <div>
+          <img className="" src={beanBrewImage } />
+      
+    </div>
+  )
+}
+
+export default Nav
